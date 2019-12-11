aa4fcec692d8e1a257cef68b8ef800fb50a223cc
vismay969 <vismayrshah@gmail.com>
2019-12-11 13:01:01 +0530

    Created Components - Header, Footer, SearchFlight and Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { SearchflightComponent } from './searchflight/searchflight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SearchflightComponent
   ],
   imports: [
     BrowserModule
