66f8babc513c27cffaa9d63a36619f093569056e
vismay969 <vismayrshah@gmail.com>
2019-12-17 10:41:47 +0530

    Vismay Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,9 @@ import { LogoutComponent } from './logout/logout.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { DisplayflightscheduleComponent } from './displayflightschedule/displayflightschedule.component';
 import { EditflightscheduleComponent } from './editflightschedule/editflightschedule.component';
-import {ModalModule} from 'ngx-bootstrap/modal';
 import { AirportinfoComponent } from './airportinfo/airportinfo.component';
 import { ViewflightmasterComponent } from './viewflightmaster/viewflightmaster.component';
+import {ModalModule} from 'ngx-bootstrap';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
